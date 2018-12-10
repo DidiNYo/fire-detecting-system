8adfa7e3f1289601c5f4a8419097391c4eb0a04e
diyanany <diyana.n.yordanova@gmail.com>
2018-12-10 23:51:05 +0200

    Removed unused code

--- fire-preventing-system/fire-preventing-system/MainWindow.xaml.cs
+++ fire-preventing-system/fire-preventing-system/MainWindow.xaml.cs
@@ -18,24 +18,21 @@ namespace fire_preventing_system
 
             //MainMap - name of the map. Defined in MainWindow.xaml
             points.Setup(MainMap);
-
-            Mapsui.Geometries.Point point = new Mapsui.Geometries.Point();
-            DataContext = point;
         }
 
         private void Btn_ClickSaveCoords(object sender, System.Windows.RoutedEventArgs e)
         {
             //We need to cast the sender as a XAML button element to get acces to its attributes
             var userCoordinates = sender as Button;
-            var y_coord = Convert.ToDouble(userCoordinates.DataContext.ToString());
             var x_coord = Convert.ToDouble(userCoordinates.Tag.ToString());
+            var y_coord = Convert.ToDouble(userCoordinates.DataContext.ToString());
 
 
             //We need to transfer from one coordinate reference system to another - from CRS to CRS
             MainMap.Map.Transformation = new MinimalTransformation();
 
             //Mapsui.Geometries.Point(x, y); where x is the X-axis(E-coord) and y is the Y-axis(N-coord)
-            var point =  (Mapsui.Geometries.Point)(MainMap.Map.Transformation.Transform("EPSG:4326", "EPSG:3857", new Mapsui.Geometries.Point(x_coord,y_coord)));
+            var point = (Mapsui.Geometries.Point)(MainMap.Map.Transformation.Transform("EPSG:4326", "EPSG:3857", new Mapsui.Geometries.Point(x_coord,y_coord)));
 
             //Map is centered with coordinates provided by the user
             MainMap.Navigator.CenterOn(new Mapsui.Geometries.Point(point.X, point.Y));
