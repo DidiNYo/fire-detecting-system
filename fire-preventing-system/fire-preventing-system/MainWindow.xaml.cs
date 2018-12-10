e879dfc2bc98de0c44c93e20a4c3bae3175de17f
diyanany <diyana.n.yordanova@gmail.com>
2018-12-10 23:39:56 +0200

    Map is centered with user data

--- fire-preventing-system/fire-preventing-system/MainWindow.xaml.cs
+++ fire-preventing-system/fire-preventing-system/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using BruTile.Predefined;
 using Mapsui.Layers;
 using Mapsui.Projection;
+using System;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace fire_preventing_system
 {
@@ -16,18 +18,31 @@ namespace fire_preventing_system
 
             //MainMap - name of the map. Defined in MainWindow.xaml
             points.Setup(MainMap);
+
+            Mapsui.Geometries.Point point = new Mapsui.Geometries.Point();
+            DataContext = point;
         }
 
         private void Btn_ClickSaveCoords(object sender, System.Windows.RoutedEventArgs e)
         {
+            //We need to cast the sender as a XAML button element to get acces to its attributes
+            var userCoordinates = sender as Button;
+            var y_coord = Convert.ToDouble(userCoordinates.DataContext.ToString());
+            var x_coord = Convert.ToDouble(userCoordinates.Tag.ToString());
+
+
             //We need to transfer from one coordinate reference system to another - from CRS to CRS
             MainMap.Map.Transformation = new MinimalTransformation();
 
             //Mapsui.Geometries.Point(x, y); where x is the X-axis(E-coord) and y is the Y-axis(N-coord)
-            var point =  (Mapsui.Geometries.Point)(MainMap.Map.Transformation.Transform("EPSG:4326", "EPSG:3857", new Mapsui.Geometries.Point(31.00, 30.00)));
+            var point =  (Mapsui.Geometries.Point)(MainMap.Map.Transformation.Transform("EPSG:4326", "EPSG:3857", new Mapsui.Geometries.Point(x_coord,y_coord)));
 
-            //The geographical center of the Earth is the Great Pyramid of Gizeh with coordinates 30°00′N 31°00′E
+            //Map is centered with coordinates provided by the user
             MainMap.Navigator.CenterOn(new Mapsui.Geometries.Point(point.X, point.Y));
         }
+
+
     }
+
+
 }
